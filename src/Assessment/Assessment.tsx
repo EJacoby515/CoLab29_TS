f07dd8dbdba6d80b5f92164599eb868ce1a940a9
Sarah Johnson <srjohnson4586@gmail.com>
2024-06-13 17:54:03 -0400

    fix conflicts

--- src/Assessment/Assessment.tsx
+++ src/Assessment/Assessment.tsx
@@ -1,27 +1,30 @@
-import React, { ReactElement, useState } from 'react';
+import React, { ReactElement, useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFaceSmileBeam as faFaceSmileBeamSolid, 
+import { faFaceSmile as faFaceSmileSolid, 
           faFaceMeh as faFaceMehSolid,
-         faFaceFrownOpen as faFaceFrownSolid} from '@fortawesome/free-solid-svg-icons';
-import { faFaceSmileBeam as faFaceSmileBeamLine, 
+          faFaceFrownOpen as faFaceFrownSolid,
+          faFaceLaugh as faFaceLaughSolid} from '@fortawesome/free-solid-svg-icons';
+import { faFaceSmile as faFaceSmileLine, 
           faFaceMeh as faFaceMehLine, 
-          faFaceFrownOpen as faFaceFrownOpenLine} from '@fortawesome/free-regular-svg-icons';
+          faFaceFrownOpen as faFaceFrownOpenLine,
+          faFaceLaugh as faFaceLaughLine} from '@fortawesome/free-regular-svg-icons';
 
 const Assessment: React.FC<{ onAssessmentSubmit: () => void }> = ({onAssessmentSubmit}) => {
   const [journal, setJournal] = useState('');
-  const [emoji, setEmoji] = useState('');
+  const [emoji, setEmoji] = useState(0);
   const [submitHovered, setsubmitHovered] = useState(false);
+  const [assessSubmitDisabled, setDisabled] = useState(false);
 
   const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setJournal(e.target.value);
   }
 
-  const submitAssessment = async (anemoji:string) => {
-    console.log({journal});
+  const submitAssessment = async (rating:number) => {
+    const reflection = journal;
     const formattedDate = new Date().toISOString();
     try {
       const response = await new Promise((resolve, reject) => {
-        chrome.runtime.sendMessage({ action: "appendStorage", key: formattedDate, value: anemoji}, (response) => {
+        chrome.runtime.sendMessage({ action: "appendAssessment", date: formattedDate, rating: rating, reflection: reflection}, (response) => {
           if (chrome.runtime.lastError) {
             reject(chrome.runtime.lastError);
           } else {
@@ -32,7 +35,7 @@ const Assessment: React.FC<{ onAssessmentSubmit: () => void }> = ({onAssessmentS
       console.log(response);
       onAssessmentSubmit();
       } catch (error) {
-        console.error('Error:', error);
+        console.log('Error:', error);
       };
   };
 
@@ -46,9 +49,9 @@ const Assessment: React.FC<{ onAssessmentSubmit: () => void }> = ({onAssessmentS
     display: 'flex',
     flexDirection: 'column',
     width: '400px',
-    backgroundColor: 'white',
-    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.3)',
-    alignItems: 'center'
+    backgroundColor: '#F8F9FF',
+    alignItems: 'center',
+    borderRadius: '12px',
   } as React.CSSProperties;
 
   const emojiContainerStyle = {
@@ -68,64 +71,77 @@ const Assessment: React.FC<{ onAssessmentSubmit: () => void }> = ({onAssessmentS
   } as React.CSSProperties;
 
   const textareaStyle ={
-    font: 'Inter',
-    height: '80px',
+    fontFamily: 'Roboto',
+    height: '120px',
     width: '100%',
-    padding: '5px',
+    padding: '12px 14px',
     borderRadius: '2px',
-    placeholder: 'Enter a short description on whether you achieved your goals'
+    border: '1px solid #D0D5DD',
+    fontSize: '14px'
   } as React.CSSProperties;
 
-  const editBtnStyle = {
-    color: 'white',
-    backgroundColor: '#3a2723',
-    borderRadius: '4px'
-  }
-
-  const submitAssessStyle = {
-    color: 'black',
+  const assessmentBtnStyle = {
+    color: submitHovered ? 'white' : '#38608F',
+    backgroundColor: submitHovered ? '#38608F' : 'transparent',
+    borderRadius: '4px',
     font: 'Inter',
     padding: '0px 12px',
     height: '32px',
     width: '100px',
     marginTop: '10px',
     textAlign: 'center',
-    border: 'none',
+    border: assessSubmitDisabled ? 'none' : '1px solid #D0D5DD',
     outline: 'none',
-    cursor: 'pointer',
-    textDecoration: submitHovered ? 'underline' : 'none'
+    cursor: assessSubmitDisabled ? null : 'pointer',
   } as React.CSSProperties;
 
   const emojiStyle = {
-    backgroundColor: 'white',
+    backgroundColor: 'transparent',
     padding: '0 10px',
     color: 'blue',
     cursor: 'pointer',
     fontSize: '20px'
   }
 
-  const emojiHover = {
-    backgroundColor: 'gray'
-  }
+  useEffect(()=>{
+    if (emoji === 0) {
+      setDisabled(true);
+    } else {
+      setDisabled(false);
+    }
+  }, [emoji])
+
 
   return (
     <>
       <div style={{...assessmentcontainerStyle}}>
-        <p style={{
-              ...pStyle,
-              textAlign: 'center'
-            }}>How did it go?</p>
+      <p style={{...pStyle}}>Rate your session:</p>
         <div style={{...emojiContainerStyle}}>
-          <a style={{...emojiStyle, color: 'green'}} onClick={()=>{setEmoji('smile')}}>
-            <FontAwesomeIcon icon={emoji === 'smile' ? faFaceSmileBeamSolid : faFaceSmileBeamLine}/></a>
-          <a style={{...emojiStyle, color: 'orange'}} onClick={()=>{setEmoji('meh')}}>
-            <FontAwesomeIcon icon={emoji === 'meh' ? faFaceMehSolid : faFaceMehLine} /></a>
-          <a style={{...emojiStyle, color: 'red'}} onClick={()=>{setEmoji('frown')}}>
-            <FontAwesomeIcon icon={emoji === 'frown' ? faFaceFrownSolid : faFaceFrownOpenLine} /></a>
+          <a style={{...emojiStyle, color: '#E31B54'}} onClick={()=>{setEmoji(1)}}>
+            <FontAwesomeIcon icon={emoji === 1 ? faFaceFrownSolid : faFaceFrownOpenLine} /></a>
+          <a style={{...emojiStyle, color: '#FF692E'}} onClick={()=>{setEmoji(2)}}>
+            <FontAwesomeIcon icon={emoji === 2 ? faFaceMehSolid : faFaceMehLine} /></a>
+          <a style={{...emojiStyle, color: '#EAAA08'}} onClick={()=>{setEmoji(3)}}>
+            <FontAwesomeIcon icon={emoji === 3 ? faFaceSmileSolid : faFaceSmileLine}/></a>
+          <a style={{...emojiStyle, color: '#66C61C'}} onClick={()=>{setEmoji(4)}}>
+            <FontAwesomeIcon icon={emoji === 4 ? faFaceSmileSolid : faFaceSmileLine}/></a>
+          <a style={{...emojiStyle, color: '#099250'}} onClick={()=>{setEmoji(5)}}>
+            <FontAwesomeIcon icon={emoji === 5 ? faFaceLaughSolid : faFaceLaughLine}/></a>
         </div>
         <p style={{...pStyle}}>What went well and what could be better?</p>
-        <textarea style={{...textareaStyle}} value={journal} onChange={handleChange}/>
-        <button style={{...editBtnStyle, fontSize: '10px'}} onMouseEnter={()=> {setsubmitHovered(true)}} onMouseLeave={() => {setsubmitHovered(false)}} onClick={() => {submitAssessment(emoji)}}>Log Session</button>
+        <textarea 
+          style={{...textareaStyle}} 
+          value={journal} 
+          onChange={handleChange}
+          placeholder="Add summary here"/>
+        <button 
+          style={{...assessmentBtnStyle, fontSize: '14px'}} 
+          onMouseEnter={()=> {setsubmitHovered(true)}} 
+          onMouseLeave={() => {setsubmitHovered(false)}} 
+          onClick={() => {submitAssessment(emoji)}}
+          disabled={assessSubmitDisabled}>
+            Submit
+        </button>
       </div>
     </>
   );
